e4277ca14d3bdaef5d7cb06c10937f2664d3c5a6
Seyvaq Singh <seyvaq@gmail.com>
2023-07-13 18:39:23 +0800

    Initial Commit

---
+++ js/access.js
@@ -0,0 +1,8 @@
+document.getElementById("btnSubmit").onclick = function() {
+    var codeInputValue = document.getElementById("codeInput").value;
+    
+    if (codeInputValue === "120722") {
+      window.location.href = "./index.html";
+    }
+  };
+  
---
+++ js/main.js
@@ -0,0 +1,4 @@
+document.getElementById("chatButton").onclick = function() {
+    document.getElementById("chatBody").classList.toggle("d-block");
+    document.getElementById("chatBody").classList.toggle("d-none");
+};
